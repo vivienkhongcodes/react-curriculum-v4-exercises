8b303adb69c77c4c470697a8ba4537443f2db399
vivienkhongcodes <264569188+vivienkhongcodes@users.noreply.github.com>
2026-07-06 00:53:18 -0400

    Complete Lesson 01 About Me component

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Vivien';
+  const age = 'a non-traditional student';
+  const hobbies = ['Zumba', 'Reading', 'Languages', 'Birding', 'Movies'];
   return (
     <div>
+      <h1>About Me</h1>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <p>
+        {' '}
+        Salve! My name is {name}. I am {age} with a background in education and
+        e-commerce, and am so excited to be learning React. It is indeed a dream
+        come true! Besides coding, I love to explore cultures by learning
+        languages as well as new technologies through Code the Dream. In
+        addition, my hobbies are:{' '}
+      </p>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
